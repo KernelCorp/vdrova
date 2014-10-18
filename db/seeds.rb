222d9eea903a569a9ee47f3dfe17b2945614a8dd
aleksey <saleksey92@gmail.com>
2014-10-18 16:47:48 +0700

    #vd-1 in progress work: 3h 40m

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 AdminUser.create email: 'admin@example.com', password: 'password'
 
-root = OrdinaryCms::Page.find_or_create_by name: 'root'
+root = OrdinaryCms::Page.create name: 'root'
 root.update_attribute :root, true
 
 text_page = OrdinaryCms::Factories::Page.create name: 'text_page'
@@ -9,7 +9,7 @@ text_page.build(name: 'Уголь').save
 text_page.build(name: 'Брикеты').save
 text_page.build(name: 'Дрова').save
 
-contacts = OrdinaryCms::Page.find_or_create_by name: 'contacts'
+contacts = OrdinaryCms::Page.create name: 'contacts'
 contacts.sections.build(name: 'info')
 contacts.sections.build(name: 'address')
 contacts.sections.build(name: 'phone')
