9d088d8f7f0a0b80e3203b339b1be74a9f3f66eb
aleksey <saleksey92@gmail.com>
2014-10-21 19:05:41 +0700

    #vd-1 images to png

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ root = OrdinaryCms::Page.create name: 'root'
 if root.valid?
   root.update_attribute :root, true
   root.sections.build(name: 'quot_text', content: 'Дерево – один из самых древних и экологически чистых видов топлива. Солнечная энергия накапливается в древесине, а затем дрова отдают ее в виде тепла. Дрова активно используются для отопления домов, бань, а также как экологическое топливо для каминов.')
-  root.sections.build(name: 'quot_img', content: '<img src="/system/images/drova.jpg">')
+  root.sections.build(name: 'quot_img', content: '<img src="/system/images/drova.png">')
   root.sections.build(name: 'title', content: 'По дрова')
   root.sections.build(name: 'body', content: '<p>Добро пожаловать к нам по дрова</p>')
   root.save
@@ -21,17 +21,17 @@ if text_page.valid?
 
   page = text_page.build(name: 'Уголь')
   page.section('quot_text').content = 'Уголь был первым из используемых человеком видов ископаемого топлива. Он позволил совершить промышленную революцию, которая в свою очередь способствовала развитию угольной промышленности, обеспечив её более современной технологией.'
-  page.section('quot_img').content = '<img src="/system/images/ughol.jpg">'
+  page.section('quot_img').content = '<img src="/system/images/ughol.png">'
   page.save
 
   page = text_page.build(name: 'Брикеты')
   page.section('quot_text').content = 'Продукт произведенный из натуральной угольной пыли, без химических добавок и склеивающих веществ с использованием инновационной технологии брикетирования.'
-  page.section('quot_img').content = '<img src="/system/images/briket.jpg">'
+  page.section('quot_img').content = '<img src="/system/images/briket.png">'
   page.save
 
   page = text_page.build(name: 'Дрова')
   page.section('quot_text').content = 'Дерево – один из самых древних и экологически чистых видов топлива. Солнечная энергия накапливается в древесине, а затем дрова отдают ее в виде тепла. Дрова активно используются для отопления домов, бань, а также как экологическое топливо для каминов. Запах, который они дают нельзя ни с чем спутать - приятный аромат горящей древесины, еле слышное потрескивание мгновенно создают атмосферу покоя и уюта.'
-  page.section('quot_img').content = '<img src="/system/images/drova.jpg">'
+  page.section('quot_img').content = '<img src="/system/images/drova.png">'
   page.save
 end
 
